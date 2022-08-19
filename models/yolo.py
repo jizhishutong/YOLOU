6d5ebfe39b08e6d202bf91ea651d08f9e7e00f4b
fzwwj95 <448051349@qq.com>
2022-08-19 10:06:02 +0800

    add space_to_depth to yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -312,7 +312,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
         RepResX, RepResXCSPA, RepResXCSPB, RepResXCSPC, Ghost, GhostCSPA, GhostCSPB, GhostCSPC,
         SwinTransformerBlock, STCSPA, STCSPB, STCSPC,
         SwinTransformer2Block, ST2CSPA, ST2CSPB, ST2CSPC,
-        conv_bn_relu_maxpool, Shuffle_Block, RepVGGBlock, CBH, LC_Block, Dense, DWConvblock, ShuffleNetV2x, DWConvblockX):
+        conv_bn_relu_maxpool, Shuffle_Block, RepVGGBlock, CBH, LC_Block, Dense, DWConvblock, ShuffleNetV2x, DWConvblockX, space_to_depth):
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
                 c2 = make_divisible(c2 * gw, 8)
